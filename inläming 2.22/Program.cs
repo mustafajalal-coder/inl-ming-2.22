fba0d1d9a397dd3f3b4cfd26bf9c1cd2b0179d93
muja07001 <muja07001@E-PW05CGR9>
2024-10-03 14:29:00 +0200

    Add project files.

---
+++ inläming 2.22/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inläming_2._22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hur långt hoppade elin ( i meter)? ");
+            double elinshopp = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("hur långt hoppade alma (i meter) ");
+            double almashopp = Convert.ToDouble(Console.ReadLine());
+
+            if (elinshopp > almashopp)
+            {
+                double skillnad = elinshopp - almashopp;
+
+                Console.WriteLine($"elin hoppade {skillnad} meter längre än alma.");
+
+
+
+
+
+
+
+
+            }
+
+
+
+        }
+    }
+}
